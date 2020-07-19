0b77d77461708a0ff5e20419fe126d17b25057c3
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-07-19 16:42:40 -0600

    js

--- DAW/Guia1/rectangulo.js
+++ DAW/Guia1/rectangulo.js
@@ -4,8 +4,25 @@
 * Archivo: rectangulo.js *
 * Uso: Calcular el área de un rectángulo. *
 * * * * * * * * * * * * * * * * * * * * * * * * * * */
+var ver;
 var base = prompt('Introduzca la base del rectángulo','');
+do{
+    if (isNaN(base)==1) {
+        ver = 0;
+        base = prompt('Solo se aceptan numeros xd:','');
+    }else{
+        ver = 1;
+    }    
+}while(ver==0);
 var altura = prompt('Introduzca la altura del rectángulo','');
+do{
+    if (isNaN(altura)==1) {
+        ver = 0;
+        altura = prompt('Solo se aceptan numeros xd:','');
+    }else{
+        ver = 1;
+    }    
+}while(ver==0);
 var area;
 area = base*altura
 document.write("<header><h1>El área del rectángulo es: " + area + "</h1><hr/><br /></header>");
