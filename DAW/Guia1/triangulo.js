6c0be39e35d0c661e3888ddfca2898fbc5bc3683
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-07-19 16:45:46 -0600

    js

--- DAW/Guia1/triangulo.js
+++ DAW/Guia1/triangulo.js
@@ -4,8 +4,25 @@
 * Archivo: triangulo.js *
 * Uso: Calcular el área de un triángulo. *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+var ver;
 var base = prompt('Introduzca la base del triángulo','');
+do{
+    if (isNaN(base)==1) {
+        ver = 0;
+        base = prompt('Solo se aceptan numeros xd:','');
+    }else{
+        ver = 1;
+    }    
+}while(ver==0);
 var altura = prompt('Introduzca la altura del triángulo','');
+do{
+    if (isNaN(altura)==1) {
+        ver = 0;
+        altura = prompt('Solo se aceptan numeros xd:','');
+    }else{
+        ver = 1;
+    }    
+}while(ver==0);
 var area;
 area = (base*altura)/2;
 document.write("<header><h1>El área del Triángulo es: " + area + "</h1><hr/><br /></header>");
