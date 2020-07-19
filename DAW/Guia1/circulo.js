cbbf115d55789b6414d23638c3a58a5a9085bc5c
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-07-19 16:41:49 -0600

    js

--- DAW/Guia1/circulo.js
+++ DAW/Guia1/circulo.js
@@ -4,8 +4,17 @@
 * Archivo: circulo.js *
 * Uso: Calcular el área de un círculo. *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+var ver;
 const PI = 3.1415926535;
 var radio = prompt('Introduzca el radio del círculo:','');
+do{
+    if (isNaN(radio)==1) {
+        ver = 0;
+        radio = prompt('Solo se aceptan numeros:','');
+    }else{
+        ver = 1;
+    }    
+}while(ver==0);
 var area;
 area = PI*radio*radio;
 document.write("<header><h1>El área del círculo es: " + area + "</h1><hr/><br /></header>");
