9e55faaf0e470a752f80fa30b9229528c85c1e83
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-07-19 19:40:06 -0600

    js

--- DAW/Guia1/grados.js
+++ DAW/Guia1/grados.js
@@ -29,21 +29,18 @@ if(cels>=41){
     menu=4;
 }
 far=(cels * (9/5)) + 32;
+document.write("<h1>La temperatura es "+far+" °F</h1>");
 switch(menu){
     case 1:
-        document.write("<h1>La temperatura es "+far+" °F</h1>");
         document.write("<img src='IMG/antartida.jpg' alt='#'>");
         break;
     case 2:
-        document.write("<h1>La temperatura es "+far+" °F</h1>");
         document.write("<img src='IMG/canada.jpg' alt='#'>");
         break;
     case 3:
-        document.write("<h1>La temperatura es "+far+" °F</h1>");
         document.write("<img src='IMG/selva.jpg' alt='#'>");
         break;
     case 4:
-        document.write("<h1>La temperatura es "+far+" °F</h1>");
         document.write("<img src='IMG/descierto.jpg' alt='#'>");
         break;
     }
