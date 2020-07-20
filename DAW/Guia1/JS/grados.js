ee8061219e85e1c3207a741d1ddd2306cc2a5f13
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-07-19 22:15:35 -0600

    js

--- DAW/Guia1/JS/grados.js
+++ DAW/Guia1/JS/grados.js
@@ -32,15 +32,15 @@ far=(cels * (9/5)) + 32;
 document.write("<h1>La temperatura es "+far+" °F</h1>");
 switch(menu){
     case 1:
-        document.write("<img src='IMG/antartida.jpg' alt='#'>");
+        document.write("<img src='../IMG/antartida.jpg' alt='#'>");
         break;
     case 2:
-        document.write("<img src='IMG/canada.jpg' alt='#'>");
+        document.write("<img src='../IMG/canada.jpg' alt='#'>");
         break;
     case 3:
-        document.write("<img src='IMG/selva.jpg' alt='#'>");
+        document.write("<img src='../IMG/selva.jpg' alt='#'>");
         break;
     case 4:
-        document.write("<img src='IMG/descierto.jpg' alt='#'>");
+        document.write("<img src='../IMG/descierto.jpg' alt='#'>");
         break;
     }
