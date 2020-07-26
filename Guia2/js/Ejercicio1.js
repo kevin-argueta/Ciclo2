243ceced1f839a02e220d73b567a63bfadfa7dd4
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-07-26 15:12:39 -0600

    guia2

--- Guia2/js/Ejercicio1.js
+++ Guia2/js/Ejercicio1.js
@@ -1,6 +1,6 @@
 function inicializar(){
     //Inicialización de variables
-    var i, contenido="", num, Cant, CantPar, CantImpar, Suma, SumaImpar, SumaPar, Mayor, Menor;
+    var i=0, contenido="", num, Cant=0, CantPar=0, CantImpar=0, Suma=0, SumaImpar=0, SumaPar=0, Mayor=0, Menor=10;
     var cifra = new array();
     do {
     num = prompt("Ingrese un numero (valor entero):", "");
@@ -20,8 +20,16 @@ function inicializar(){
     contenido += "<h1>cantidad de Cifras</h1>\n";
     //Lazo que muestra la cantidad de cifras
     do{
-
-    }while();
+       cifra[i]=num%10;
+       num=num/10;
+       i++;
+       cant++;
+    }while(num<10);
+    if(num!=0){
+    cifra[i]=num;
+    cant++;
+    }
+    contenido += "<h2>"+cant+"</h2>\n";
     datos.innerHTML = contenido;
    }
    window.onload = inicializar;
