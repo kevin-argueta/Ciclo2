fa904d364562e9e07bd03cafa68ce91a08dcdeb7
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-09-27 20:29:59 -0600

    guia9

--- guia9/js/ejercicio1.js
+++ guia9/js/ejercicio1.js
@@ -23,123 +23,141 @@ function init(){
         }
     }
     var request = new ajaxRequest();
-    var peli1= document.getElementById("1");
-    var peli2= document.getElementById("2");
-    var peli3= document.getElementById("3");
-    var peli4= document.getElementById("4");
-    var peli5= document.getElementById("5");
-    var peli6= document.getElementById("6");
+    var eleccion1= document.getElementById("img1");
+    var eleccion2= document.getElementById("img2");
+    var eleccion3= document.getElementById("img3");
+    var eleccion4= document.getElementById("img4");
+    var eleccion5= document.getElementById("img5");
+    var eleccion6= document.getElementById("img6");
     request.onreadystatechange = function(){
         if(request.readyState==4){
             if(request.status==200 || window.location.href.indexOf("http")==-1){
             //Recibir resultado como un objeto de JavaScript usando la función eval()
             //var jsondata = eval("("+request.responseText+")");
             //Recibir resultado como un objeto de JavaScript usando el método parse()
-                if(peli1.addEventListener){
-                    peli1.addEventListener("click", function () {
+                if(eleccion1.addEventListener){
+                    eleccion1.addEventListener("click", function () {
                         var jsondata = JSON.parse(request.responseText);
                         var rssentries = jsondata.items;
                         var output = "<ul>";
                             var i=0;
                             output += "<li>";
                             output += "<h3>" + rssentries[i].title1 + "</h3>";
-                            output += "<p>" + rssentries[i].pro1 + "</p>";
-                            output += "<p>" + rssentries[i].description1 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].protagonista1 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].descripcion1 + "</p>";
+                            output += "<br>";
                             output += "<p>" + rssentries[i].director1 + "</p>";
                             output += "</a>";
                             output += "</li>";
                         output += "</ul>";
-                        document.getElementById("result").innerHTML = output
+                        document.getElementById("mostrar").innerHTML = output
                         return false;
                     }, false);
                 }
-                if(peli2.addEventListener){
-                    peli2.addEventListener("click", function () {
+                if(eleccion2.addEventListener){
+                    eleccion2.addEventListener("click", function () {
                         var jsondata = JSON.parse(request.responseText);
                         var rssentries = jsondata.items;
                         var output = "<ul>";
                             var i=1;
                             output += "<li>";
                             output += "<h3>" + rssentries[i].title2 + "</h3>";
-                            output += "<p>" + rssentries[i].pro2 + "</p>";
-                            output += "<p>" + rssentries[i].description2 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].protagonista2 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].descripcion2 + "</p>";
+                            output += "<br>";
                             output += "<p>" + rssentries[i].director2 + "</p>";
                             output += "</a>";
                             output += "</li>";
                         output += "</ul>";
-                        document.getElementById("result").innerHTML = output
+                        document.getElementById("mostrar").innerHTML = output
                         return false;
                     }, false);
                 }
-                if(peli3.addEventListener){
-                    peli3.addEventListener("click", function () {
+                if(eleccion3.addEventListener){
+                    eleccion3.addEventListener("click", function () {
                         var jsondata = JSON.parse(request.responseText);
                         var rssentries = jsondata.items;
                         var output = "<ul>";
                             var i=2;
                             output += "<li>";
                             output += "<h3>" + rssentries[i].title3 + "</h3>";
-                            output += "<p>" + rssentries[i].pro3 + "</p>";
-                            output += "<p>" + rssentries[i].description3 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].protagonista3 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].descripcion3 + "</p>";
+                            output += "<br>";
                             output += "<p>" + rssentries[i].director3 + "</p>";
                             output += "</a>";
                             output += "</li>";
                         output += "</ul>";
-                        document.getElementById("result").innerHTML = output
+                        document.getElementById("mostrar").innerHTML = output
                         return false;
                     }, false);
                 }
-                if(peli4.addEventListener){
-                    peli4.addEventListener("click", function () {
+                if(eleccion4.addEventListener){
+                    eleccion4.addEventListener("click", function () {
                         var jsondata = JSON.parse(request.responseText);
                         var rssentries = jsondata.items;
                         var output = "<ul>";
                             var i=3;
                             output += "<li>";
                             output += "<h3>" + rssentries[i].title4 + "</h3>";
-                            output += "<p>" + rssentries[i].pro4 + "</p>";
-                            output += "<p>" + rssentries[i].description4 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].protagonista4 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].descripcion4 + "</p>";
+                            output += "<br>";
                             output += "<p>" + rssentries[i].director4 + "</p>";
                             output += "</a>";
                             output += "</li>";
                         output += "</ul>";
-                        document.getElementById("result").innerHTML = output
+                        document.getElementById("mostrar").innerHTML = output
                         return false;
                     }, false);
                 }
-                if(peli5.addEventListener){
-                    peli5.addEventListener("click", function () {
+                if(eleccion5.addEventListener){
+                    eleccion5.addEventListener("click", function () {
                         var jsondata = JSON.parse(request.responseText);
                         var rssentries = jsondata.items;
                         var output = "<ul>";
                             var i=4;
                             output += "<li>";
                             output += "<h3>"+ rssentries[i].title5 + "</h3>";
-                            output += "<p>" + rssentries[i].pro5 + "</p>";
-                            output += "<p>" + rssentries[i].description5 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].protagonista5 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].descripcion5 + "</p>";
+                            output += "<br>";
                             output += "<p>" + rssentries[i].director5 + "</p>";
                             output += "</a>";
                             output += "</li>";
                         output += "</ul>";
-                        document.getElementById("result").innerHTML = output
+                        document.getElementById("mostrar").innerHTML = output
                         return false;
                     }, false);
                 }
-                if(peli6.addEventListener){
-                    peli6.addEventListener("click", function () {
+                if(eleccion6.addEventListener){
+                    eleccion6.addEventListener("click", function () {
                         var jsondata = JSON.parse(request.responseText);
                         var rssentries = jsondata.items;
                         var output = "<ul>";
                             var i=5;
                             output += "<li>";
                             output += "<h3>" + rssentries[i].title6 + "</h3>";
-                            output += "<p>" + rssentries[i].pro6 + "</p>";
-                            output += "<p>" + rssentries[i].description6 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].protagonista6 + "</p>";
+                            output += "<br>";
+                            output += "<p>" + rssentries[i].descripcion6 + "</p>";
+                            output += "<br>";
                             output += "<p>" + rssentries[i].director6 + "</p>";
                             output += "</a>";
                             output += "</li>";
                         output += "</ul>";
-                        document.getElementById("result").innerHTML = output
+                        document.getElementById("mostrar").innerHTML = output
                         return false;
                     }, false);
                 }
