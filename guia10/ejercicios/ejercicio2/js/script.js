905d8b93961928f9ff0da9276b04df9a16365200
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-10-06 08:52:44 -0600

    guias LIC

---
+++ guia10/ejercicios/ejercicio2/js/script.js
@@ -0,0 +1,15 @@
+prettyPrint();
+
+$('#ascensorBuilding').ascensor({
+    AscensorName: 'ascensor',
+    ChildType: 'section',
+    AscensorFloorName: 'Home | Implementation | HTML | Jquery | CSS | Smartphone | Credits | hola  | salu  | como',
+    Time: 1000,
+    WindowsOn: 1,
+    Direction: 'chocolate',
+    AscensorMap: '2|1 & 2|2 & 3|3 & 2|3 & 2|4 & 1|4 & 2|5 & 1|3 & 1|2 & 3|4',
+    Easing: 'easeInOutQuad',
+    KeyNavigation: true,
+    Queued: false,
+
+});
