f3aef736bd1cc218cd2adf609a13f238a2d3028a
kevin-argueta <67917203+kevin-argueta@users.noreply.github.com>
2020-10-13 10:05:39 -0600

    guia11

--- guia11/sueldos/src/app/empleado/empleado.component.ts
+++ guia11/sueldos/src/app/empleado/empleado.component.ts
@@ -14,15 +14,24 @@ export class EmpleadoComponent implements OnInit {
   nombre:string;
   base:number;
   neto:number;
+  isss:number;
+  renta:number;
+  afp:number;
   constructor() { }
   ngOnInit(){
     this.nombre='';
     this.base=0;
     this.neto=0;
+    this.isss=0;
+    this.renta=0;
+    this.afp=0;
   }
   ingresar(){
-    this.neto=this.base-((this.base*0.074)+(this.base*0.11)+(this.base*0.051));
-    this.sueldos={"nombre":this.nombre,"base":this.base,"neto":this.neto};
+    this.isss=(this.base*0.074);
+    this.renta=(this.base*0.11);
+    this.afp=(this.base*0.051);
+    this.neto=this.base-(this.isss+this.renta+this.afp);
+    this.sueldos={"nombre":this.nombre,"base":this.base,"neto":this.neto,"isss":this.isss,"renta":this.renta,"afp":this.afp};
     this.registro.push(this.sueldos);
   }
 }
